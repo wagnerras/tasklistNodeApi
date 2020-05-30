d0a5a0a3a1e9f09c64d73c6e634c23bec84c2aa1
unknown <wagnerras@gmail.com>
2020-05-30 19:19:28 -0300

    bscrypt

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
     static init(sequelize) {
@@ -6,13 +7,25 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING
             },
             {
                 sequelize,
             }
         );
+
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+
+        return this;
     }
+
+
+
 }
 
 export default User;
